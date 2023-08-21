5b3c18984e9bfb14387302f6034c85578bbeaef8
Alberto Nao <alnao@alnao.it>
2023-08-21 17:50:37 +0200

    Ec2InstanceWindow su classe e metodi per tag e startStop

--- awsPyConsole/main.py
+++ awsPyConsole/main.py
@@ -9,66 +9,75 @@ from tkinter import ttk
 
 #see example tk https://realpython.com/python-gui-tkinter/
 
-def main_window_start():
-    root = tk.Tk()
-    #strvar = StringVar(root, name ="str") #see https://www.geeksforgeeks.org/python-setting-and-retrieving-values-of-tkinter-variable/
-    #root.setvar(name ="profilo_selezionato", value ="loading")#profilo_selezionato=StringVar(root,name="profilo_selezionato",value="loading")
-    # Create window
-    root.title('Aws Py Console')
-    x=10#root.winfo_screenwidth() // 6
-    y=10#int(root.winfo_screenheight() * 0.1)
-    root.geometry ('1000x700+' + str(x) + "+" + str(y) ) #WIDTHxHEIGHT+TOP+LEFT
+class AwsPyConsole:
+    def __init__(self,):
+        self.profilo='default'
+        self.root = tk.Tk()
+        #strvar = StringVar(root, name ="str") #see https://www.geeksforgeeks.org/python-setting-and-retrieving-values-of-tkinter-variable/
+        #root.setvar(name ="profilo_selezionato", value ="loading")#profilo_selezionato=StringVar(root,name="profilo_selezionato",value="loading")
+        # Create window
+        self.root.title('Aws Py Console')
+        x=10#root.winfo_screenwidth() // 6
+        y=10#int(root.winfo_screenheight() * 0.1)
+        self.root.geometry ('1000x700+' + str(x) + "+" + str(y) ) #WIDTHxHEIGHT+TOP+LEFT
+        #get AWS profiles
+        lista_profili_aws=AwsProfiles.get_lista_profili()
+        #crete menu
+        menu.create_menu(self.root,lista_profili_aws,self.load_profile)
+        self.main_frame(self.root,"")
+        self.root.mainloop()
+        
+    def add_text_to_frame(self,frame,text):
+        Label(frame, text=text).pack()
+        frame.pack_propagate(False)
 
-    #get AWS profiles
-    lista_profili_aws=AwsProfiles.get_lista_profili()
-
-    #crete menu
-    menu.create_menu(root,lista_profili_aws,reload_profile)
-    main_frame(root,"")
-    root.mainloop()
-    
-def add_text_to_frame(frame,text):
-    Label(frame, text=text).pack()
-    frame.pack_propagate(False)
-
-def main_frame(root,profilo): #nota: funziona solo se c'è un iframe nella main
-    #if '!frame' in root.children: #nota: funziona solo se c'è un iframe nella main
-    #    print ( root.children )
-    if profilo=="": #nessun profilo selezionato dal menu
-        frame1=tk.Frame(root,width=975,height=675,bg="#EEEEEE")
-        frame1.grid(row=0,column=0)
-        add_text_to_frame(frame1,"Selezionare un profilo") # Label(frame1, text="Selezionare un profilo").pack()
+    def main_frame(self,root,profilo): #nota: funziona solo se c'è un iframe nella main
+        self.profilo=profilo
+        if self.profilo=="": #nessun profilo selezionato dal menu
+            self.frame1=tk.Frame(root,width=975,height=685,bg="#EEEEEE")
+            self.frame1.grid(row=0,column=0)
+            self.add_text_to_frame(self.frame1,"Selezionare un profilo") # Label(frame1, text="Selezionare un profilo").pack()
+            #frame1.pack_propagate(False)
+            return
+        self.frame1=tk.Frame(root,width=975,height=675,bg="#EEEEEE")
+        self.frame1.grid(row=0,column=0)
+        #tabs window
+        self.tabs = ttk.Notebook(self.frame1, width=950, height=670)
+        self.tabs.pack(fill=BOTH, expand=TRUE)
+        self.frame2a = ttk.Frame(self.tabs)
+        self.frame2b = ttk.Frame(self.tabs)#.grid(row=1, columnspan=2)
+        self.tabs.add(self.frame2a, text="Profilo " + self.profilo)
+        self.add_text_to_frame(self.frame2a,"TODO" + self.profilo)
+        self.frame2a.pack_propagate(False)
+        self.tabs.add(self.frame2b, text="Ec2")
+        self.add_text_to_frame(self.frame2b,"Lista istanze del profilo "+self.profilo)
+        self.load_ec2_instance_window()
+        self.tabs.pack(expand=1, fill="both")
+        #frame1=tk.Frame(root,width=975,height=675,bg="#EEEEEE")
+        #frame1.grid(row=0,column=0)
+        #l = Label(frame1, text="Profilo selezionato: " + profilo).pack()
         #frame1.pack_propagate(False)
-        return
-    frame1=tk.Frame(root,width=975,height=675,bg="#EEEEEE")
-    frame1.grid(row=0,column=0)
-    #tabs 
-    tabs = ttk.Notebook(frame1, width=950, height=650)
-    #style = ttk.Style()
-    #style.theme_settings("default", {"TNotebook.Tab": {"configure": {"padding": [30, 30]}}})
-    tabs.pack(fill=BOTH, expand=TRUE)
-    frame1 = ttk.Frame(tabs)
-    frame2 = ttk.Frame(tabs)
-    tabs.add(frame1, text="Profilo " + profilo)
-    add_text_to_frame(frame1,"TODO" + profilo)
-    tabs.add(frame2, text="Ec2")
-    add_text_to_frame(frame2,"Lista istanze del profilo "+profilo)
-    ec2_instances.crea_lista_istanze(frame2,profilo,AwsInstances.get_lista_istanze(profilo))
-    tabs.pack(expand=1, fill="both")
-    #frame1=tk.Frame(root,width=975,height=675,bg="#EEEEEE")
-    #frame1.grid(row=0,column=0)
-    #l = Label(frame1, text="Profilo selezionato: " + profilo).pack()
-    #frame1.pack_propagate(False)
 
-def reload_profile(root,profilo):
-    main_frame(root,profilo)
-    #root.setvar(name ="profilo_selezionato",value =profilo)
-    #print(profilo)
+    def load_ec2_instance_window(self):
+        self.frame2b.pack_propagate(False)
+        ec2_instances.Ec2InstanceWindow(self.frame2b,self.profilo
+            ,AwsInstances.get_lista_istanze(self.profilo)
+            ,AwsInstances.set_tag
+            ,AwsInstances.stop_instance
+            ,AwsInstances.start_instance
+            ,self.reload_ec2_instance_window )
 
-def main():
-    main_window_start()
+    def reload_ec2_instance_window(self):
+        print ("reload_ec2_instance_window")
+        for widget in self.frame2b.winfo_children():
+            widget.destroy()
+        #self.frame2b.pack_forget() # or frm.grid_forget() depending on whether the frame was packed or grided. #self.frame2.Destroy()
+        #self.frame2b = ttk.Frame(self.tabs)
+        self.load_ec2_instance_window()
+        #self.tabs.pack(expand=1, fill="both")
 
+    def load_profile(self,root,profilo):
+        self.main_frame(root,profilo)
 
 if __name__ == '__main__':
-    main()
-
+    AwsPyConsole()
--- awsPyConsole/sdk/ec2_instances.py
+++ awsPyConsole/sdk/ec2_instances.py
@@ -3,13 +3,39 @@ import boto3
 #see https://github.com/awsdocs/aws-doc-sdk-examples/tree/main/python/example_code/ec2#code-examples
 
 #see https://boto3.amazonaws.com/v1/documentation/api/latest/guide/ec2-example-managing-instances.html
-
+#see #https://medium.com/featurepreneur/using-ec2-services-using-boto3-ad5453fe3bea
 
 def get_lista_istanze(profile_name):
     boto3.setup_default_session(profile_name=profile_name)
     ec2 = boto3.client('ec2')
     response = ec2.describe_instances()
     return response
+
+def set_tag(instance_id, tag_key, tag_value):
+    if tag_key=='':
+        return
+    ec2 = boto3.resource('ec2')# region_name=AWS_REGION)
+    tags=[{'Key': tag_key,'Value': tag_value}]
+    instances = ec2.instances.filter(InstanceIds=[instance_id,],)
+    for instance in instances:
+        instance.create_tags(Tags=tags)
+    print("set_tag "  + instance_id)
+    return tags
+
+#https://medium.com/featurepreneur/using-ec2-services-using-boto3-ad5453fe3bea
+def stop_instance(instance_id):
+    ec2_client = boto3.client('ec2')#, region_name=”us-west-2"
+    print ("stop_instance " + instance_id)
+    response = ec2_client.stop_instances(InstanceIds=[instance_id])
+    return response
+
+def start_instance(instance_id):
+    ec2_client = boto3.client('ec2')
+    print ("start_instance " + instance_id)
+    response = ec2_client.start_instances(InstanceIds=[instance_id])
+    return response
+
+#def terminate_instance(instance_id):
 
 def main():
     print("Aws Py Console - Ec2 Instances START")
